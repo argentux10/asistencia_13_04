e616dfc3d1c599ae4e3de75534e2d44ec359bf00
Ezequiel-PC <Ezequiel-PC@DESKTOP-SELTJ5M>
2024-04-13 13:09:27 -0300

    Se crea el paquete para el ejercicio 11 de la continuacion del tp1 y clase Main

---
+++ ContinuacionTP1_Asistencia_13_04/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int numero = 80;
+
+	}
+
+}
---
+++ ContinuacionTP1_Asistencia_13_04/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Ezequiel-PC
+ *
+ */
+module ContinuacionTP1_Asistencia_13_04 {
+}
