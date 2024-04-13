5ec726da985029cd0003bc740c5e4258298d6970
Ezequiel-PC <Ezequiel-PC@DESKTOP-SELTJ5M>
2024-04-13 13:16:40 -0300

    Se resolvio el ejercicio 11 del tp1

--- ContinuacionTP1_Asistencia_13_04/src/ar/edu/unju/fi/ejercicio11/Main.java
+++ ContinuacionTP1_Asistencia_13_04/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		int numero = 80;
-
+		for (int i=2, j=40;j>0;i++,j--) {
+			System.out.println(numero + " ");
+			numero+=(j-i-1);
+		}
 	}
 
 }
